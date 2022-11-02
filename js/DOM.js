ad6347f5b2673d5ddbe3788532eb8a30d816983f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-02 12:11:46 +0600

    added a class named friend to the div of new item

--- js/DOM.js
+++ js/DOM.js
@@ -1,5 +1,5 @@
 document.getElementById('apply-bg').addEventListener('click', function () {
-    const friends = document.getElementsByClassName('frined');
+    const friends = document.getElementsByClassName('friend');
     for (const friend of friends) {
         friend.style.backgroundColor = 'pink';
     }
@@ -13,6 +13,7 @@ document.getElementById('center-third').addEventListener('click', function () {
 document.getElementById('new-friend').addEventListener('click', function () {
     const friendContainer = document.getElementById('friends');
     const newFriend = document.createElement('div');
+    newFriend.classList.add('friend');
     newFriend.innerHTML = `
     <h3 class="freind-name">New Friend</h3>
     <p>Added new friend in this page</p>
