b7ec9cfd5ba0357acfa1b0935c0dec36e6dcf560
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-02 12:05:43 +0600

    selected total friendContainer and created some necessary html tags for adding new friend element

--- js/DOM.js
+++ js/DOM.js
@@ -11,5 +11,11 @@ document.getElementById('center-third').addEventListener('click', function () {
 });
 
 document.getElementById('new-friend').addEventListener('click', function () {
-    console.log('clicked');
+    const friendContainer = document.getElementById('friends');
+    const newFriend = document.createElement('div');
+    newFriend.innerHTML = `
+    <h3 class="freind-name">New Friend</h3>
+    <p>Added new friend in this page</p>
+    `
+    friendContainer.appendChild(newFriend);
 })
