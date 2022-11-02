8c3819b9ef3d7ce737b727479ea139ed073f6595
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-02 11:50:47 +0600

    added a btn named center the third

--- js/DOM.js
+++ js/DOM.js
@@ -1,22 +1,11 @@
 document.getElementById('apply-bg').addEventListener('click', function () {
-    const friends = document.getElementsByClassName('friend');
+    const friends = document.getElementsByClassName('frined');
     for (const friend of friends) {
-        friend.style.backgroundColor = 'lightblue';
+        friend.style.backgroundColor = 'pink';
     }
 });
 
 document.getElementById('center-third').addEventListener('click', function () {
     const third = document.getElementById('third-friend');
     third.style.textAlign = 'center';
-});
-
-document.getElementById('add-friend').addEventListener('click', function () {
-    const friendContainer = document.getElementById('friends');
-    const friend = document.createElement('div');
-    friend.classList.add('friend');
-    friend.innerHTML = `
-    <h3 class="friend-name">New Friend</h3>
-    <p>Added new friend</p>
-    `
-    friendContainer.appendChild(friend);
 })
