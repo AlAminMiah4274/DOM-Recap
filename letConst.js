31237f80fcd70332afcafd796591e321f715199b
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-03 22:05:57 +0600

    use of const in array

--- letConst.js
+++ letConst.js
@@ -1,2 +1,13 @@
+let money = 25;
+money = 36;
+console.log(money);
+
 const bird = 'jan pakhi';
-console.log(bird);
+const message = bird + ' potas potas';
+console.log(message);
+
+const numbers = [12, 45, 78, 154, 186];
+numbers.push(56, 785);
+numbers[2] = 80;
+numbers[0] = 10;
+console.log(numbers);
