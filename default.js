96eecd86e4aa6f50e27b756672a8767402fa0b18
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-05 17:17:59 +0600

    discussed about default parameter

---
+++ default.js
@@ -0,0 +1,14 @@
+function add(first, second = 0) {
+    console.log(first, second);
+    const total = first + second;
+    return total;
+}
+const result = add(10);
+console.log(result);
+
+function fullName(first, last = 'Chowdhury') {
+    const name = first + ' ' + last;
+    return name;
+}
+const name = fullName('Gelam');
+console.log(name);
