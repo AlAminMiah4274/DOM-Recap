b1cf38be68343da5ae3cdbbfca0d5d5cfce8d6aa
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-06 22:15:34 +0600

    discussed about destructuring and spread operator togeather

--- destructuring.js
+++ destructuring.js
@@ -12,22 +12,22 @@ const fish = {
 
 const { phone, price, color, name } = fish;
 
-console.log(name);
-console.log(price);
-console.log(color);
-console.log(phone);
+// console.log(name);
+// console.log(price);
+// console.log(color);
+// console.log(phone);
 
 const { age, profession } = { name: 'Almas', age: 56, profession: 'makeup artist' };
-console.log(age);
+// console.log(age);
 
 // array destructuring:
 const [first, second] = [45, 80, 90, 97, 54, 85];
-console.log(first, second);
+// console.log(first, second);
 
 // mojar jinis:
 const hero = ['Cris Hamswarth', 'Cris Evans', 'Tom Cruise'];
 const [thor, captain, mission] = hero;
-console.log(thor, mission);
+// console.log(thor, mission);
 
 // another destructuring:
 function getNames() {
@@ -35,4 +35,10 @@ function getNames() {
 }
 
 const [baba, chacha] = getNames();
-console.log(chacha, baba);
+// console.log(chacha, baba);
+
+// destructurig & spread operator:(*****)
+const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4, c: 5, d: 6 };
+console.log(x);
+console.log(y);
+console.log(z);
